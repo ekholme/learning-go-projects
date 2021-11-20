275f254958669d6dd6bc77c2647d2e3636a0c98b
Eric Ekholm <eric.ekholm@gmail.com>
2021-11-20 07:08:39 -0500

    finish basic rest api

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -26,6 +26,8 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID) //note that the colon preceding the path signifies that id is a parameter
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -33,4 +35,35 @@ func main() {
 //getAlbums creates JSON from the slice of albums, writing out a JSON response
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+//postAlbums adds an album from JSON received in the request body.
+//other note -- go doesn't really care about the order in which we declare functions; just that we use them if we declare them
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+//getAlbumByID locates the album whose ID value matches the id
+//parameter sent by the client, then returns that album as a response
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	//loop over list of albums, looking for an album that matches the parameter
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
