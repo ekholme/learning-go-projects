f77b04ca4f25c8842023337e20d005f8dfabd4e0
Eric Ekholm <eric.ekholm@gmail.com>
2021-11-23 11:46:51 -0500

    finish wiki proj

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -2,76 +2,140 @@ package main
 
 import (
 	"html/template"
+
 	"log"
+
 	"net/http"
+
 	"os"
+
+	"regexp"
 )
 
-//defining data structures
 type Page struct {
 	Title string
-	Body  []byte //note this is a byte rather than a string bc that's the type expected by the io libraries
+
+	Body []byte
 }
 
-//read this as 'this is a method named save that takes as its receiver p, a pointer to Page. It takes
-//no parameters and returns a value of type error
-//it returns an error bc it will return nil if everything goes well, and will only return anything if something goes wrong
 func (p *Page) save() error {
+
 	filename := p.Title + ".txt"
+
 	return os.WriteFile(filename, p.Body, 0600)
+
 }
 
 func loadPage(title string) (*Page, error) {
+
 	filename := title + ".txt"
+
 	body, err := os.ReadFile(filename)
-	if err != nil { //this will return nil if we load a page and error otherwise
+
+	if err != nil {
+
 		return nil, err
+
 	}
-	return &Page{Title: title, Body: body}, nil //this will return a pointer to a Page literal constructed with title and body values
+
+	return &Page{Title: title, Body: body}, nil
+
 }
 
-//creating a template render function
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
-}
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 
-//this lets us view a page
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
-	//if statement below will redirect a user to an edit page if the page doesn't exist
+
 	if err != nil {
+
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+
 		return
+
 	}
+
 	renderTemplate(w, "view", p)
+
 }
 
-//editHandler will let us edit pages
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+
 	p, err := loadPage(title)
+
 	if err != nil {
+
 		p = &Page{Title: title}
+
 	}
+
 	renderTemplate(w, "edit", p)
+
 }
 
-//saveHandler will handle the submission of forms located on edit pages
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+
 	body := r.FormValue("body")
+
 	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
+
+	err := p.save()
+
+	if err != nil {
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+
+	}
+
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+
 }
 
-//RESUME AT 'ERROR HANDLING' here: https://golang.org/doc/articles/wiki/
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+
+	if err != nil {
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+	}
+
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		m := validPath.FindStringSubmatch(r.URL.Path)
+
+		if m == nil {
+
+			http.NotFound(w, r)
+
+			return
+
+		}
+
+		fn(w, r, m[2])
+
+	}
+
+}
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
