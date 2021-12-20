5d92c5284ff96455adb7cf82054c4948f3c6f7c2
Eric Ekholm <eric.ekholm@gmail.com>
2021-12-20 14:43:16 -0500

    finish link parser

---
+++ gophercises-link-parser/README.md
@@ -0,0 +1,3 @@
+# Link Parser
+
+This project creates a link parser following the Gophercises tutorial [here](https://courses.calhoun.io/lessons/les_goph_16)
---
+++ gophercises-link-parser/parse.go
@@ -0,0 +1,68 @@
+package link
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+//represents a link in an html document
+type Link struct {
+	Href string
+	Text string
+}
+
+//function Parse takes in an io.Reader and returns a slice of links or an error
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	nodes := linkNodes(doc)
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+		fmt.Println(node)
+	}
+	return links, nil
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+		linkNodes(c)
+	}
+	return ret
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = text(n)
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c) + " "
+	}
+	return strings.Join(strings.Fields(ret), " ")
+}
