d47dd5372422bec1b873d31a1148ac1d31cc7a0e
Eric Ekholm <eric.ekholm@gmail.com>
2022-01-09 06:56:55 -0500

    finish cmd shell

--- cli-task-manager/README.md
+++ cli-task-manager/README.md
@@ -1,3 +1,5 @@
 A CLI TODO list/task manager tool. Created following the tutorial [here](https://courses.calhoun.io/lessons/les_goph_35)
 
-RESUME AT 7:45 IN CLI SHELL VID
+RESUME AT 17:30 IN CLI SHELL VID
+
+Note that this needs to be called in the terminal using the dir name: "cli-task-manager"
---
+++ cli-task-manager/cmd/add.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Adds a task to the task list",
+	Run: func(cmd *cobra.Command, args []string) {
+		task := strings.Join(args, " ")
+
+		fmt.Printf("Added \"%s\" to your task list.\n", task)
+	},
+}
+
+//init() is a function that gets run before the main function gets run -- they set up things
+//that need to be set up before an app runs
+func init() {
+	RootCmd.AddCommand(addCmd)
+}
